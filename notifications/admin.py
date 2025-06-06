b8ced5bec8f7308b743fb4262e3cfabf3ae59516
parsard <parsaroodhastam@gmail.com>
2025-06-06 16:54:55 +0330

    activitylog admin add .

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import ActivityLog
 from .models import Notification
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
     list_display = ('user', 'message', 'created_at')
+
+@admin.register(ActivityLog)
+class ActivityLogAdmin(admin.ModelAdmin):
+    list_display = ("timestamp", "user", "path", "method", "status_code", "ip_address")
+    search_fields = ("user__username", "path", "ip_address")
+    list_filter = ("method", "status_code", "user")
+    ordering = ("-timestamp",)
