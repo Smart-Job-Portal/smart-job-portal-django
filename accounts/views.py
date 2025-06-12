fed456c030fd25c654cf8b06d756f97f429023f5
parsard <parsaroodhastam@gmail.com>
2025-06-12 16:57:49 +0330

    edit and view profile path added.

--- accounts/views.py
+++ accounts/views.py
@@ -3,22 +3,22 @@ from django.contrib.auth import login
 from .forms import CustomUserCreationForm
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.core.mail import EmailMessage
 from .tokens import account_activation_token
 from django.http import HttpResponse
-from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth import get_user_model
 from core.utils import send_welcome_email
 from core.tasks import send_activation_email_task, send_welcome_email_task
-from django.contrib.auth.views import PasswordResetView
 
+from django.contrib.auth.views import PasswordResetView
 from django.contrib import messages
 
+# ------------------ Registration View ------------------
 def register_view(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST, request.FILES if request.FILES else None)
         if form.is_valid():
             user = form.save(commit=False)
             user.is_active = False
@@ -27,17 +27,15 @@ def register_view(request):
             domain = get_current_site(request).domain
             send_activation_email_task.delay(user.id, domain)
 
-
-            # ===== Inform user to check their email via Django messages =====
             messages.info(request, "Please check your email to activate your account before logging in.")
             return render(request, 'accounts/please_check_email.html')
         else:
-            #show error for invalid form
             messages.error(request, "There was a problem with your registration. Please correct the errors below.")
     else:
         form = CustomUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
 
+# ------------------ Account Activation View ------------------
 def activate_account(request, uidb64, token):
     try:
         uid = urlsafe_base64_decode(uidb64).decode()
@@ -51,13 +49,47 @@ def activate_account(request, uidb64, token):
         login(request, user)
 
         send_welcome_email_task.delay(user.id)
-        send_welcome_email(user)
+        try:
+            send_welcome_email(user)
+        except Exception:
+            pass
 
-        #Inform user on success
         messages.success(request, "Your account has been activated! You are now logged in.")
         return redirect('dashboard')
     else:
-        #  Inform user on invalid/expired link 
         messages.error(request, "Activation link is invalid or has expired. Please register again or contact support if you need help.")
         return redirect('login')
-  
+
+# ============================================================
+#         ADD YOUR PROFILE VIEW FUNCTION HERE!
+# ============================================================
+
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def profile_view(request):
+    """
+    Display the current user's profile page.
+    """
+    profile = request.user.userprofile
+    return render(request, 'accounts/profile.html', {'profile': profile})
+
+
+
+from .forms import UserProfileForm
+
+@login_required
+def profile_edit_view(request):
+    profile = request.user.userprofile
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your profile has been updated!")
+            return redirect('accounts:profile')
+        else:
+            messages.error(request, "Please correct the errors below.")
+    else:
+        form = UserProfileForm(instance=profile)
+    return render(request, 'accounts/profile_edit.html', {'form': form, 'profile': profile})
+
